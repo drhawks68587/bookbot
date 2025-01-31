ff56a9ca6927ee8a96a58b47c48a85dad0e7a634
drhawks68587 <dillon.ross87@gmail.com>
2025-01-31 16:01:35 -0500

    final

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
+def sort_on(dict):
+    return dict["count"]
 def main():
     charcount = {}
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
         words = file_contents.split()
-        count = len(words)
-        print(count)
+        word_count = len(words)
     lowercase = file_contents.lower()
     for char in lowercase:
         if char in charcount:
             charcount[char] += 1
         else:
             charcount[char] = 1
-    print(charcount)
+    char_list = []
+    for char , count in charcount.items():
+        if char.isalpha():
+            char_dict = {"char": char, "count": count}
+            char_list.append(char_dict)
+    char_list.sort(reverse=True, key=sort_on)
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{word_count} words found in the document")
+    for char_dict in char_list:
+        print(f"The '{char_dict['char']}' character was found {char_dict['count']} times")
+    print("--- End report ---")
+
+    
 
 
 
